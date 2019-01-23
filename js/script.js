f59c6b267fe88f46be91c3a910de810e698960b0
Palina Asavets <palina.asavets.gmail.com>
2019-01-23 20:46:15 +0300

    добавлены часы в js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,29 @@
 window.onload = function () {
     function clock() {
-        var date = new date {},
-            day = date.
+        var date = new Date(),
+            day = date.getDate(),
+            month = date.getMonth(),
+            monthArr = ["JUN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"],
+            year = date.getFullYear(),
+            hour = date.getHours(),
+            min = date.getMinutes(),
+            sec = date.getSeconds();
 
+        if (day < 10) day = "0" + day;
+        if (hour < 10) hour = "0" + hour;
+        if (min < 10) min = "0" + min;
+        if (sec < 10) sec = "0" + sec;
+
+        document.getElementById("time-date").innerHTML = day + " " + monthArr[month] + " " + year;
+        document.getElementById("time-hour").innerHTML = hour + ":" + min + ":" + sec;
     }
 
+    var timer;
+
+    function clockStart() {
+        timer = setInterval(clock, 1000);
+        clock()
+    }
+
+    clockStart();
 }
